5bd30b6b25760258dad7921a31569bd7788c94bd
Romain Tixier <127192511+rtix141@users.noreply.github.com>
2024-11-20 14:55:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,26 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+  
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
   
 if __name__ == "__main__":
   app.run(debug=True)
